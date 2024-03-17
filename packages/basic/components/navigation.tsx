be1c897d5f8604f73f82c3892fed8e4208d679cc
Jaepil Yu <jaepilyu1324@gmail.com>
2024-03-17 17:57:43 +0900

    hydration

--- packages/basic/components/navigation.tsx
+++ packages/basic/components/navigation.tsx
@@ -1,15 +1,22 @@
 "use client"
+// use client: client에서만 렌더링된다는 의미 x
+// backend에서 render되고, client에 hydrate 된다.
 
 import Link from "next/link"
 import {usePathname} from "next/navigation"
+import {useState} from "react";
 
 export default function Navigation() {
     const path = usePathname()
+    const [count, setCount] = useState<number>(0)
 
     return <nav>
         <ul>
             <li><Link href="/">Home</Link> {path === '/' ? '🔥' : ""}</li>
             <li><Link href="/about-us">About Us</Link>{path === '/about-us' ? '🔥' : ""}</li>
+            <li>
+                <button onClick={() => setCount(c => c + 1)}>{count}</button>
+            </li>
         </ul>
     </nav>
 }
