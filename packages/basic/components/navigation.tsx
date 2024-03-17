a2cd80ca4fd0f92dc84a18cb430a2be47792293e
Jaepil Yu <jaepilyu1324@gmail.com>
2024-03-17 23:23:56 +0900

    Styling

--- packages/basic/components/navigation.tsx
+++ packages/basic/components/navigation.tsx
@@ -5,14 +5,14 @@
 import Link from "next/link"
 import {usePathname} from "next/navigation"
 import {useState} from "react";
-import styles from "./navigation.module.css"
+import styles from "@styles/navigation.module.css"
 
 export default function Navigation() {
     const path = usePathname()
     const [count, setCount] = useState<number>(0)
 
     return <nav className={styles.nav}>
-        <ul className={styles.list}>
+        <ul>
             <li><Link href="/">Home</Link> {path === '/' ? '🔥' : ""}</li>
             <li><Link href="/about-us">About Us</Link>{path === '/about-us' ? '🔥' : ""}</li>
             <li>
