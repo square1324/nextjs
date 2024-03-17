bc924b48969500bbe15432ccc0ea5c2380346436
Jaepil Yu <jaepilyu1324@gmail.com>
2024-03-17 20:43:22 +0900

    Client Side fetch

--- packages/basic/app/(home)/page.tsx
+++ packages/basic/app/(home)/page.tsx
@@ -1,8 +1,22 @@
-import Navigation from "@components/navigation";
+'use client'
+import {useEffect, useState} from "react";
 
 const Page = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [movies, setMovies] = useState([])
+    const getMovies = async () => {
+        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+        const json  = await response.json()
+        setMovies(json)
+        setIsLoading(false)
+    }
+    useEffect(() => {
+        getMovies()
+    }, [])
+
+
     return <div>
-        <h1>Hello!</h1>
+        <h1>{isLoading ? "Loading.." : JSON.stringify(movies)}</h1>
     </div>
 }
 
