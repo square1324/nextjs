7375e65654cc94474f7078e3705c12133bb55a18
Jaepil Yu <jaepilyu1324@gmail.com>
2024-03-19 00:42:58 +0900

    prettier 설정

--- packages/style/app/page.tsx
+++ packages/style/app/page.tsx
@@ -1,8 +1,10 @@
-import variables from './page.module.scss'
+import variables from "./page.module.scss";
 
 export default function Page() {
-    return <div>
-        <h1 style={{color: variables.primaryColor}}>Hello, Next.js!</h1>
-        <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+  return (
+    <div>
+      <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
     </div>
+  );
 }
