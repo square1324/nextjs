abf3973b5ceead4bdf0bbb7408882b0bf2f78ed1
Jaepil Yu <jaepilyu1324@gmail.com>
2024-03-19 00:34:05 +0900

    tailwindcss 설정

--- packages/style/app/page.tsx
+++ packages/style/app/page.tsx
@@ -1,5 +1,8 @@
 import variables from './page.module.scss'
 
 export default function Page() {
-    return <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+    return <div>
+        <h1 style={{color: variables.primaryColor}}>Hello, Next.js!</h1>
+        <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    </div>
 }
