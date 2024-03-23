3feec9bb58713a7bb7ef326ce3200b04d60aa271
Jaepil Yu <jaepilyu1324@gmail.com>
2024-03-24 00:31:31 +0900

    typography - 1

---
+++ packages/style/app/typography/fontsize/page.tsx
@@ -0,0 +1,89 @@
+export default function Page() {
+  return (
+    <div className='m-20'>
+      <h1 className='text-xs'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-sm'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-lg'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-2xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-3xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-4xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-5xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-6xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-7xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-8xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-9xl'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-[15px]'>Tailwind CSS 폰트 사이즈</h1>
+      <h1 className='text-3xl italic'>Tailwind CSS Font Style(폰트 기울림)</h1>
+      <em className='text-6xl not-italic'>Tailwind CSS Font Style(폰트 기울림)</em>
+      <h1 className='text-2xl italic font-semibold'>Tailwind CSS Font Weight(폰트 굵기)</h1>
+      <h1 className='text-3xl font-bold tracking-widest'>Tailwind CSS Class</h1>
+      <hr className='m-10' />
+      <div className='text-[15px] font-semibold tracking-tight line-clamp-2'>
+        국회의원은 국가이익을 우선하여 양심에 따라 직무를 행한다. 법관은 헌법과 법률에 의하여 그 양심에 따라 독립하여 심판한다. 법관이 중대한 심신상의 장해로
+        직무를 수행할 수 없을 때에는 법률이 정하는 바에 의하여 퇴직하게 할 수 있다. 군인·군무원·경찰공무원 기타 법률이 정하는 자가 전투·훈련등 직무집행과
+        관련하여 받은 손해에 대하여는 법률이 정하는 보상외에 국가 또는 공공단체에 공무원의 직무상 불법행위로 인한 배상은 청구할 수 없다. 대통령은 법률에서
+        구체적으로 범위를 정하여 위임받은 사항과 법률을 집행하기 위하여 필요한 사항에 관하여 대통령령을 발할 수 있다. 대통령은 제3항과 제4항의 사유를 지체없이
+        공포하여야 한다. 국민경제자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다. 제1항의 지시를 받은 당해 행정기관은 이에 응하여야 한다. 국가는
+        법률이 정하는 바에 의하여 재외국민을 보호할 의무를 진다. 계엄을 선포한 때에는 대통령은 지체없이 국회에 통고하여야 한다. 국가는 농업 및 어업을
+        보호·육성하기 위하여 농·어촌종합개발과 그 지원등 필요한 계획을 수립·시행하여야 한다. 모든 국민은 통신의 비밀을 침해받지 아니한다. 감사위원은 원장의
+        제청으로 대통령이 임명하고, 그 임기는 4년으로 하며, 1차에 한하여 중임할 수 있다. 대통령의 국법상 행위는 문서로써 하며, 이 문서에는 국무총리와 관계
+        국무위원이 부서한다. 군사에 관한 것도 또한 같다. 모든 국민은 학문과 예술의 자유를 가진다.
+      </div>
+      <hr className='m-10' />
+      <div className='leading-[2rem]'>
+        국회의원은 국가이익을 우선하여 양심에 따라 직무를 행한다. 법관은 헌법과 법률에 의하여 그 양심에 따라 독립하여 심판한다. 법관이 중대한 심신상의 장해로
+        직무를 수행할 수 없을 때에는 법률이 정하는 바에 의하여 퇴직하게 할 수 있다. 군인·군무원·경찰공무원 기타 법률이 정하는 자가 전투·훈련등 직무집행과
+        관련하여 받은 손해에 대하여는 법률이 정하는 보상외에 국가 또는 공공단체에 공무원의 직무상 불법행위로 인한 배상은 청구할 수 없다. 대통령은 법률에서
+        구체적으로 범위를 정하여 위임받은 사항과 법률을 집행하기 위하여 필요한 사항에 관하여 대통령령을 발할 수 있다. 대통령은 제3항과 제4항의 사유를 지체없이
+        공포하여야 한다. 국민경제자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다. 제1항의 지시를 받은 당해 행정기관은 이에 응하여야 한다. 국가는
+        법률이 정하는 바에 의하여 재외국민을 보호할 의무를 진다. 계엄을 선포한 때에는 대통령은 지체없이 국회에 통고하여야 한다. 국가는 농업 및 어업을
+        보호·육성하기 위하여 농·어촌종합개발과 그 지원등 필요한 계획을 수립·시행하여야 한다. 모든 국민은 통신의 비밀을 침해받지 아니한다. 감사위원은 원장의
+        제청으로 대통령이 임명하고, 그 임기는 4년으로 하며, 1차에 한하여 중임할 수 있다. 대통령의 국법상 행위는 문서로써 하며, 이 문서에는 국무총리와 관계
+        국무위원이 부서한다. 군사에 관한 것도 또한 같다. 모든 국민은 학문과 예술의 자유를 가진다.
+      </div>
+      <hr className='m-10' />
+      <div className='text-[20px]'>
+        div 안의 테스트
+        <div className='leading-[1.6em]'>
+          국회의원은 국가이익을 우선하여 양심에 따라 직무를 행한다. 법관은 헌법과 법률에 의하여 그 양심에 따라 독립하여 심판한다. 법관이 중대한 심신상의 장해로
+          직무를 수행할 수 없을 때에는 법률이 정하는 바에 의하여 퇴직하게 할 수 있다. 군인·군무원·경찰공무원 기타 법률이 정하는 자가 전투·훈련등 직무집행과
+          관련하여 받은 손해에 대하여는 법률이 정하는 보상외에 국가 또는 공공단체에 공무원의 직무상 불법행위로 인한 배상은 청구할 수 없다. 대통령은 법률에서
+        </div>
+        <hr className='m-10' />
+        <div className='leading-[1.6rem]'>
+          국회의원은 국가이익을 우선하여 양심에 따라 직무를 행한다. 법관은 헌법과 법률에 의하여 그 양심에 따라 독립하여 심판한다. 법관이 중대한 심신상의 장해로
+          직무를 수행할 수 없을 때에는 법률이 정하는 바에 의하여 퇴직하게 할 수 있다. 군인·군무원·경찰공무원 기타 법률이 정하는 자가 전투·훈련등 직무집행과
+          관련하여 받은 손해에 대하여는 법률이 정하는 보상외에 국가 또는 공공단체에 공무원의 직무상 불법행위로 인한 배상은 청구할 수 없다. 대통령은 법률에서
+        </div>
+      </div>
+
+      <hr className='m-10' />
+      <ul className='border border-black list-decimal list-inside'>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+      </ul>
+
+      <hr className='m-10' />
+      <ul className='list-[lower-alpha]'>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+      </ul>
+
+      <hr className='m-10' />
+      <ul className="list-image-[url('/checkbox.png')]">
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+        <li> 안녕하세요.</li>
+      </ul>
+    </div>
+  )
+}
